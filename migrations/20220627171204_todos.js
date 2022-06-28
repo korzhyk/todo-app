9fec2a50deab9c0baa7c3834810b518b10f63060
Andrii Korzh <Andrii.Korzh@gmail.com>
2022-06-28 15:42:09 +0300

    migrations to cjs
    prevent to use any typescript compilers in production

--- migrations/20220627171204_todos.js
+++ migrations/20220627171204_todos.js
@@ -1,14 +1,10 @@
-import type { Knex } from 'knex'
-
-export async function up(knex: Knex): Promise<void> {
+module.exports.up = async function up(knex) {
   return knex.schema.createTable('todos', (table) => {
     table.increments('id').primary()
     table.string('title', 255).notNullable()
     table.boolean('complete')
   })
 }
-
-export async function down(knex: Knex): Promise<void> {
+module.exports.down = async function down(knex) {
   return knex.schema.dropTable('todos')
 }
-
