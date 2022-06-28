ac6dc50f325fee4b3883a31e23401d05c2e59ba2
Andrii Korzh <Andrii.Korzh@gmail.com>
2022-06-28 15:16:22 +0300

    Update create-todo.dto.ts

--- src/todos/dto/create-todo.dto.ts
+++ src/todos/dto/create-todo.dto.ts
@@ -1,1 +1,10 @@
-export class CreateTodoDto {}
+import { IsBoolean, IsNotEmpty, IsOptional } from 'class-validator'
+
+export class CreateTodoDto {
+  @IsNotEmpty()
+    title: string
+
+  @IsOptional()
+  @IsBoolean()
+    complete: boolean
+}
