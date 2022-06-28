40c9748430255974f1f04a687f01b28fd2cce32e
Andrii Korzh <Andrii.Korzh@gmail.com>
2022-06-28 14:53:49 +0300

    allow to change default app port

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,6 @@ if (!process.env.NODE_ENV)
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
-  await app.listen(3000)
+  await app.listen(+process.env.PORT || 3000)
 }
 bootstrap()
